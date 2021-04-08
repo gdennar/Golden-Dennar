3ba302d327f52fca0a48744a281fe69891e7e783
gdennar <79317964+gdennar@users.noreply.github.com>
2021-04-08 16:09:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.html').animate({width:'70%'},3000)
+    $('.css').animate({width:'60%'},3000)
+    $('.js').animate({width:'40%'},3000)
+    $('.uix').animate({width:'50%'},3000)
+    
+})
