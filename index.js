8489746433c55345dbc538c2b063cec67711bcfc
Golden Dennar <79317964+gdennar@users.noreply.github.com>
2022-08-09 16:02:09 +0100

    update index.js

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ $(document).ready(function () {
     $('.html').animate({width:'80%'},3000)
     $('.css').animate({width:'75%'},3000)
     $('.js').animate({width:'60%'},3000)
-    $('.react').animate({width:'50%'},3000)
     
 })
